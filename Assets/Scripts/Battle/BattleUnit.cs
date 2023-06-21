ae5a40befebbf4cf0be33e1a3b32b011a1bd49b2
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-06-21 16:11:33 +0200

    Introduccion plugin DOT (Animaciones)
    El personaje entra al escenario con una animacion y el slime igual.

--- Assets/Scripts/Battle/BattleUnit.cs
+++ Assets/Scripts/Battle/BattleUnit.cs
@@ -2,22 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class BattleUnit : MonoBehaviour
 {
     [SerializeField] EnemyBase _base;
+    [SerializeField] PlayerUnit _playerUnit;
     [SerializeField] byte level;
     [SerializeField] bool isPlayerUnit;
-     
+
     public Enemy Enemy { get; set; }
+    public PlayerUnit PlayerUnit { get { return _playerUnit; } }
+
+    Image image;
+    Vector3 originalPos;
+
+    public void Awake()
+    {
+        image = GetComponent<Image>();
+        originalPos = image.transform.localPosition;
+    }
 
     public void Setup()
     {
         Enemy = new Enemy(_base, level);
 
-        if (isPlayerUnit)
+        image.sprite = Enemy.Base.FrontSprite;
+
+        PlayerUnitEnterAnimation();
+        EnemyUnityEnterAnimation();
+    }
+
+    public void PlayerUnitEnterAnimation()
+    {
+        // Encuentra el objeto PlayerUnit en la escena
+        PlayerUnit playerUnit = FindObjectOfType<PlayerUnit>();
+
+        if (playerUnit != null)
         {
-            GetComponent<Image>().sprite = Enemy.Base.FrontSprite;
+            Vector3 originalPos = playerUnit.transform.localPosition;
+            // Mueve el objeto PlayerUnit a la posición deseada
+            playerUnit.transform.localPosition = new Vector3(-500f, playerUnit.transform.localPosition.y);
+            playerUnit.transform.DOLocalMoveX(originalPos.x, 1f);
         }
+    }
+
+    public void EnemyUnityEnterAnimation()
+    {
+        image.transform.localPosition = new Vector3(500f, originalPos.y);
+        image.transform.DOLocalMoveX(originalPos.x, 1f);
     }
 }
